0f07f0c3ba32f90495974696dd7b13e57531d293
Mahmood Saadalla <mahmoodsadalla@gmail.com>
2026-03-30 16:40:40 +0200

    Question 11

--- Question 11/AnimalShelter.cs
+++ Question 11/AnimalShelter.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace G_NET_12_Ad01.Question_11
 {
-    internal class AnimalShelter<T> where T : Animal 
+    internal class AnimalShelter<T> where T : Animal  , IComparable<T>
     {
         private List<T> animals = new();
         public void Add(T animal)
